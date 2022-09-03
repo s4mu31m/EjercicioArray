8d2e234338fd9f5654e7cecf389e25ef41542977
Luis Alvarado <luis.alvaradob@correoaiep.cl>
2022-09-03 17:51:15 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace EjerciciosDeAlmacenamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            byte i, numNotas;
+            double sumaNotas = 0, promedio;
+
+            //Pedimos al profesor el numero de Notas
+            Console.Write("Ingrese el número de notas: ");
+            numNotas = Convert.ToByte(Console.ReadLine());
+
+            //Creamos el array/arreglo/matriz unidimensional
+            double[] notas = new double[numNotas];
+
+            //Llenar el array con las notas
+            for (i = 0; i < numNotas; i++)
+            {
+                //Pedimos que ingrese la nota
+                Console.WriteLine("Ingrese la nota: ");
+                notas[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            //Calcular promedio
+
+            //Calcular nota minima
+
+            //Calcular nota maxima
+
+            //Mostrar notas
+            for (i = 0; i < numNotas; i++)
+            {
+                //Pedimos que ingrese la nota
+                Console.WriteLine("La nota {0} es : {1}", i+1, notas[i]);
+
+            }
+        }
+    }
+}
